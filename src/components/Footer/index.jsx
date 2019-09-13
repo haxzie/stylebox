ddabe45af6d39e864d973ba838af67e3cfc23019
haxzie <musthu.gm@gmail.com>
2019-09-14 01:43:25 +0530

    Add footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {Center} from '../Layout'
+import Container from '../Container'
+
+function Footer() {
+    return (
+        <Container>
+            <Center>
+                <p>Designed with ❤ by <a href="https://github.com/haxzie" target="_blank" rel="noopener">haxzie</a></p>
+            </Center>
+        </Container>
+    )
+}
+
+export default Footer
--- src/components/layout.js
+++ src/components/layout.js
@@ -10,6 +10,7 @@ import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./Header"
+import Footer from "./Footer"
 import { Column } from "./Layout";
 import Container from "./Container";
 
@@ -31,6 +32,7 @@ const Layout = ({ children }) => {
       <Container>
         {children}
       </Container>
+      <Footer/>
     </>
   )
 }
